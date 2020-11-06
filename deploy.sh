4ee3d90cb0dbe34a4c6f7e148fdd671336743720
Bhupen Pal <bhupen16pal@gmail.com>
2020-11-07 01:24:01 +0530

    Deploy Script Removed

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd client
-git subtree pull --prefix client/dist origin gh-pages
-npm run build
-echo "dscmait.xyz" > ./dist/CNAME
-git add dist && git commit -m "Initial dist subtree commit"
-cd ..
-git subtree push --prefix client/dist origin gh-pages
