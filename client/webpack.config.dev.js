754154ba7d6527c2629e5a8c3b668c540f1a7350
Bhupen Pal <bhupen16pal@gmail.com>
2020-12-27 14:16:11 +0530

    JSON support added

--- client/webpack.config.dev.js
+++ client/webpack.config.dev.js
@@ -3,70 +3,73 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
-  devServer: {
-    port: 8080,
-    disableHostCheck: true,
-    historyApiFallback: true,
-    contentBase: './',
-    hot: true,
-  },
-  entry: {
-    main: resolve(__dirname, 'src', 'index.js')
-  },
-  output: {
-    path: resolve(__dirname, 'dist'),
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.json$/,
-        use: {
-          loader: 'json-loader',
-          options: { 
-            name: '[path][name].[ext]'
-          }
-        }
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: { loader: 'babel-loader' }
-      },
-
-      {
-        test: /\.jsx$/,
-        exclude: /node_modules/,
-        use: { loader: 'babel-loader' }
-      },
-      {
-        test: /\.(s[ac]ss|css)$/i,
-        use: [ 'style-loader', 'css-loader', 'sass-loader' ],
-      },
-      {
-        test: /\.(png|jpg|jpeg|svg|gif)$/i,
-        use: {
-            loader: 'url-loader',
-            options: { 
-              limit: 15360,
-              name: '[path][name].[ext]'
-            }
-        }
-      },
-      {
-        test: /\.svg$/,
-        loader: 'svg-inline-loader'
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HTMLWebpackPlugin({
-      filename: 'index.html',
-      inject: true,
-      template: resolve(__dirname, 'src', 'index.html')
-    })
-  ]
+	mode: 'development',
+	devServer: {
+		port: 8080,
+		disableHostCheck: true,
+		historyApiFallback: true,
+		contentBase: './',
+		hot: true,
+	},
+	entry: {
+		main: resolve(__dirname, 'src', 'index.js')
+	},
+	output: {
+		path: resolve(__dirname, 'dist'),
+		filename: '[name].bundle.js',
+		chunkFilename: '[name].bundle.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.json$/,
+				type: 'javascript/dynamic',
+				use: [
+					{
+						loader: 'json-import-loader',
+						options: {
+							processPath: path => path,
+						}
+					},
+					{ loader: 'json-loader' }
+				],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: { loader: 'babel-loader' }
+			},
+			{
+				test: /\.jsx$/,
+				exclude: /node_modules/,
+				use: { loader: 'babel-loader' }
+			},
+			{
+				test: /\.(s[ac]ss|css)$/i,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.(png|jpg|jpeg|svg|gif)$/i,
+				use: {
+					loader: 'url-loader',
+					options: {
+						limit: 15360,
+						name: '[path][name].[ext]'
+					}
+				}
+			},
+			{
+				test: /\.svg$/,
+				loader: 'svg-inline-loader'
+			}
+		]
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HTMLWebpackPlugin({
+			filename: 'index.html',
+			inject: true,
+			template: resolve(__dirname, 'src', 'index.html')
+		})
+	]
 }
