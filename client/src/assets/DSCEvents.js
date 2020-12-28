c6112091eb78352f3654893e18dcca6864c28a95
Arpit Gupta <arpit.8.a@gmail.com>
2020-12-28 16:05:37 +0530

    Events page added

---
+++ client/src/assets/DSCEvents.js
@@ -0,0 +1,117 @@
+import infoSession from "./eventBanners/infoSession.jpg";
+import thirtyDays from "./eventBanners/30daysofgooglecloud.jpg";
+import android from "./eventBanners/android.jpg";
+import wow from "./eventBanners/wow.jpg";
+import xr from "./eventBanners/xr.jpg";
+import flutter from "./eventBanners/flutter.jpg";
+import hacktoberfest from "./eventBanners/hacktoberfest.jpg";
+import halloween from "./eventBanners/halloween.jpg";
+import orientation from "./eventBanners/orientation.jpg";
+import pwa from "./eventBanners/pwa.jpg";
+import cloudComputing from "./eventBanners/cloudComputing.jpg";
+
+const events = {
+  results: [
+    {
+      title: "DSC MAIT Core Team Info Session",
+      date: "02-09-2020",
+      description:
+        "Discussion of learning objectives and workflow of the community with the Core team of the Developer Students Club MAIT.",
+      link:
+        "https://dsc.community.dev/events/details/developer-student-clubs-maharaja-agrasen-institute-of-technology-delhi-presents-dsc-mait-core-team-info-session/",
+      image: infoSession,
+    },
+    {
+      title: "Orientation Session 2020-21",
+      date: "14-09-2020",
+      description:
+        "This orientation session would quench your curiosity and help you know more about DSC MAIT and the benefits it offers. We would discuss our mission, goals, and expectations, along with a brief of the technical domains we would be guiding you in. ",
+      link:
+        "https://dsc.community.dev/events/details/developer-student-clubs-maharaja-agrasen-institute-of-technology-delhi-presents-orientation-session-2020-21/",
+      image: orientation,
+    },
+    {
+      title: "30 Days of Google Cloud at MAIT",
+      date: "28-09-2020",
+      description:
+        "The 30 Days of Google Cloud program at MAIT will provide you on opportunity to kickstart your career in cloud and get hands on practice on Google Cloud Platform - the tool that powers apps like Google Search, Gmail and YouTube. ",
+      link:
+        "https://dsc.community.dev/events/details/developer-student-clubs-maharaja-agrasen-institute-of-technology-delhi-presents-30-days-of-google-cloud-at-mait/",
+      image: thirtyDays,
+    },
+    {
+      title: "Hacktoberfest 2020 - Open source for beginners",
+      date: "01-10-2020",
+      description:
+        "Welcoming everyone: Welcoming everyone on board, setting up expectations for the event, and tarting off with introductions. Introduction to Hacktoberfest: A 2-4 minute introduction to Hacktoberfest and what are the important pointers that make this event important. ",
+      link:
+        "https://dsc.community.dev/events/details/developer-student-clubs-maharaja-agrasen-institute-of-technology-delhi-presents-hacktoberfest-2020-open-source-for-beginners/",
+      image: hacktoberfest,
+    },
+    {
+      title: "Cloud Computing by Google Developer Expert",
+      date: "13-10-2020",
+      description:
+        "A tour of what cloud computing is along with a showcase of Google Cloud Platform products and services by Mr. Dharmesh Vaya, GDE. About the speaker: A Google Developer Cloud Expert and Director of Technology at Agrahyah Technologies. He is a Solutions Architect with close to 14 years of experience.",
+      link:
+        "https://dsc.community.dev/events/details/developer-student-clubs-maharaja-agrasen-institute-of-technology-delhi-presents-cloud-computing-by-google-developer-expert/",
+      image: cloudComputing,
+    },
+    {
+      title: "Progressive Web Applications",
+      date: "30-10-2020",
+      description:
+        "What and Why of Progressive Web Apps, Introduction to Js promises and Events, Service Workers Lifecycle of workers, Requirements for PWA, Installation of PWA, Caching, Live demo ",
+      link:
+        "https://dsc.community.dev/events/details/developer-student-clubs-maharaja-agrasen-institute-of-technology-delhi-presents-progressive-web-applications/",
+      image: pwa,
+    },
+    {
+      title: "Demo Day - Halloween Special",
+      date: "07-11-2020",
+      description:
+        "Projects developed by the Core members to be showcased. Halloween Specials and many more. DSC MAIT official website launch. Eligibility Criteria of members and their role to join the community.",
+      link:
+        "https://dsc.community.dev/events/details/developer-student-clubs-maharaja-agrasen-institute-of-technology-delhi-presents-demo-day-halloween-special/",
+      image: halloween,
+    },
+    {
+      title: "Building your first App with Flutter",
+      date: "21-11-2020",
+      description:
+        "Introduction to Flutter - Welcoming everyone, starting with What is Flutter and Why should one learn Flutter for developing apps, More Discussion about Flutter - Flutter popularity in recent years and organizations using Flutter, Building the App.",
+      link:
+        "https://dsc.community.dev/events/details/developer-student-clubs-maharaja-agrasen-institute-of-technology-delhi-presents-building-your-first-app-with-flutter/",
+      image: flutter,
+    },
+    {
+      title: "XR Show - Haptics Design",
+      date: "28-11-2020",
+      description:
+        "The XR Show will be an event with one goal in mind i.e. bridging the gap between communities and companies. The episode to be hosted by the CEO of Interhaptics(Paris), to share his insights about Haptics in XR.",
+      link:
+        "https://dsc.community.dev/events/details/developer-student-clubs-maharaja-agrasen-institute-of-technology-delhi-presents-xr-show-haptics-design/",
+      image: xr,
+    },
+    {
+      title: "Android Study Jam - Intro to Kotlin",
+      date: "06-12-2020",
+      description:
+        "Android Study Jams are community-organized study groups for people to learn how to build Android apps in the Kotlin programming language, using a curriculum provided by Google. There are two tracks available. New to Programming Track and Prior Programming Experience Track.",
+      link:
+        "https://dsc.community.dev/events/details/developer-student-clubs-maharaja-agrasen-institute-of-technology-delhi-presents-android-study-jam-intro-to-kotlin/",
+      image: android,
+    },
+    {
+      title: "DSC WOW | INDIA'S BIGGEST COLLABORATION | PRIZES WORTH 10 LAKHS!",
+      date: "07-12-2020",
+      description:
+        "We cannot be more excited to invite you all to be a part of this 7 Day technological celebration, hosted by more than 120 DSC chapters across India. DSC WOW is a week-long virtual event consisting of webinars & workshops for five days and the ‘CodeOffDuty’ Hackathon for the last 48 hours. ",
+      link:
+        "https://dsc.community.dev/events/details/developer-student-clubs-maharaja-agrasen-institute-of-technology-delhi-presents-dsc-wow-indias-biggest-collaboration-prizes-worth-10-lakhs/",
+      image: wow,
+    },
+  ],
+};
+
+export default events;
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React, { Component, lazy, Suspense } from "react";
 import ReactDOM from "react-dom";
-import { HashRouter,BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, BrowserRouter, Route, Switch } from "react-router-dom";
 import "./assets/css/index.scss";
 import Nav from "./Components/Nav.jsx";
 //import Home from "./Screens/Home.jsx";
@@ -10,6 +10,8 @@ import "aos/dist/aos.css";
 import Loader from "./Components/Loader.jsx";
 import Projects from "./Screens/Projects.jsx";
 //import Blogs from "./Screens/Blogs.jsx";
+import Events from "./Screens/Events.jsx";
+import "bootstrap/dist/css/bootstrap.css";
 
 const Home = lazy(() => import("./Screens/Home.jsx"));
 const Teams = lazy(() => import("./Screens/Teams.jsx"));
@@ -18,14 +20,15 @@ const Blogs = lazy(() => import("./Screens/Blogs.jsx"));
 class App extends Component {
   render() {
     return (
-      <HashRouter  basename={process.env.PUBLIC_URL}>
+      <HashRouter basename={process.env.PUBLIC_URL}>
         <Nav>
-          <Suspense fallback={<Loader/>}>
+          <Suspense fallback={<Loader />}>
             <Switch>
               <Route path="/" exact component={Home} />
               <Route path="/projects" exact component={Projects} />
               <Route path="/teams" exact component={Teams} />
               <Route path="/blogs" exact component={Blogs} />
+              <Route path="/events" exact component={Events} />
             </Switch>
           </Suspense>
         </Nav>
